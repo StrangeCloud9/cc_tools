48410760874cc401a8c960ba8045c27c07ebc5a3
StrangeCloud9 <326902993@qq.com>
2019-02-13 18:35:31 -0500

    finish first part
    load dat file and print it. Then save the content in data/pfgd_test.txt

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -4,4 +4,11 @@ import cc_dat_utils
 input_dat_file = "data/pfgd_test.dat"
 
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
+data = cc_dat_utils.make_cc_data_from_dat(input_dat_file)
 #print the resulting data
+print (data)
+#save content
+save_file_name = "data/pfgd_test.txt"
+save_file = open(save_file_name, "w")
+save_file.write(str(data))
+save_file.close()
