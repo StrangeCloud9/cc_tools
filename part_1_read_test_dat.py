c236004fe4e1a3cda67e6fddf43a0b34ff08fe0a
StrangeCloud9 <326902993@qq.com>
2019-02-22 16:21:39 -0500

    add initial codes for trans

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -6,7 +6,9 @@ input_dat_file = "data/pfgd_test.dat"
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
 data = cc_dat_utils.make_cc_data_from_dat(input_dat_file)
 #print the resulting data
+print (type(data))
 print (data)
+
 #save content
 save_file_name = "data/pfgd_test.txt"
 save_file = open(save_file_name, "w")
